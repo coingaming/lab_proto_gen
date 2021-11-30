69e32ebaa0f0a54002e6bc03a22e00dfe5788fe0
tkachuk-labs <tkachuk.labs@gmail.com>
2021-11-30 14:39:18 +0200

    fix shell.sh

--- nix/shell.sh
+++ nix/shell.sh
@@ -2,6 +2,8 @@
 
 set -e
 
+PLATFORM=`uname -m`
+
 echo "starting nixos container..."
 docker run -it --rm \
   -e NIXPKGS_ALLOW_BROKEN=1 \
@@ -9,11 +11,12 @@ docker run -it --rm \
   -v "$(pwd):/app" \
   -v "nix:/nix" \
   -v "nix-19.09-root:/root" \
-  -w "/app" nixos/nix:2.3.12 sh -c "
+  -w "/app" "heathmont/nix:alpine-$PLATFORM-2.3.15" sh -c "
   ./nix/bootstrap.sh &&
-  nix-shell ./nix/shell.nix --pure \
+  nix-shell ./nix/shell.nix \
     -I ssh-config-file=/tmp/.ssh/config \
     --argstr robotSshKey $ROBOT_SSH_KEY \
     --option sandbox false \
-    --pure -v --show-trace
+    --pure -v --show-trace \
+    $@
   "
