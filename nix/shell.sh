5f388aa828a4e517498d87face50bb2e9c7b5927
tkachuk-labs <tkachuk.labs@gmail.com>
2021-12-01 13:20:06 +0200

    use extra_substituters instead of nix copy

--- nix/shell.sh
+++ nix/shell.sh
@@ -7,6 +7,11 @@ if [ "$PLATFORM" = "aarch64" ]; then
   PLATFORM="arm64"
 fi
 
+NIX_CONF="http2 = false
+extra-substituters = https://cache.nixos.org https://hydra.iohk.io https://all-hies.cachix.org file:///app/nix_ci_cache
+trusted-public-keys = cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY= hydra.iohk.io:f/Ea+s+dFdN+3Y/G+FDgSq+a5NEWhJGzdjvKNGv0/EQ= all-hies.cachix.org-1:JjrzAOEUsD9ZMt8fdFbzo3jNAyEWlPAwdVuHw4RD43k=
+"
+
 echo "starting nixos container..."
 docker run -it --rm \
   -e NIXPKGS_ALLOW_BROKEN=1 \
@@ -15,7 +20,8 @@ docker run -it --rm \
   -v "nix:/nix" \
   -v "nix-19.09-root:/root" \
   -w "/app" "heathmont/nix:alpine-$PLATFORM-2.3.15" sh -c "
-  ./nix/bootstrap.sh &&
+  echo \"$NIX_CONF\" >> /etc/nix/nix.conf && \
+  ./nix/bootstrap.sh && \
   nix-shell ./nix/shell.nix \
     -I ssh-config-file=/tmp/.ssh/config \
     --argstr robotSshKey $ROBOT_SSH_KEY \
