f6f23c48f1965c18cdb7a0537905f1e1325f253f
Karlo Smid <777520+karlosmid@users.noreply.github.com>
2025-07-17 15:12:00 +0200

    Update lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -129,7 +129,7 @@ pub fn run() -> Result<(), Box<dyn std::error::Error>> {
         };
 
         let mut file = File::create(format!("{}.proto", dir.to_str().unwrap()))?;
-        file.write_all(protobuf_gen::ProtobufString::to_protobuf(&fd.owned()).as_bytes())?; 
+        file.write_all(protobuf_gen::ProtobufString::to_protobuf(&fd.to_owned(), fd).as_bytes())?; 
     }
 
     Ok(())
